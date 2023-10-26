95e473afa62115d881e58d69f4fd611f1010fd37
Kim Weast <weastkim3@gmail.com>
2023-10-25 20:10:56 -0500

    update

--- main.js
+++ main.js
@@ -2,67 +2,76 @@ console.log("Script attached");
 /*----------- Exercise #1: SELECTING/MANIPULATING ELEMENTS -----------*/
 
 // Select Node #1 and change the text to: "I used the getElementById("node1") method to access this"
-document.getElementById("node1").textContent = "I used the getElementById("node1") method to access this
-
+var node1 = document.getElementById('node1');
+if (node1) {
+    node1.textContent = 'I used the getElementById("node1") method to access this';
+}
 // Select Node #2 and change the text to: "I used the getElementByClassName("node2") method to access this" */
-const node2 = document.getElementsByClassName("node2")
-node2[0].textContent = 'I used the getElementByClassName("node2") method to acces this';
+var node2List = document.getElementsByClassName('node2');
+if (node2List.length > 0) {
+    for (var i = 0; i < node2List.length; i++) {
+        node2List[i].textContent = 'I used the getElementsByClassName("node2") method to access this';
+    }
+}
 
 // Select ALL the h3 tags and change the text to: "I used the getElementByTagName("h3") method to access all of these" */
-const node2 = document.getElementsByTagName("h3");
-for (let i = 0; i < h3Tags.length; i++) {
-    h3Tags[i].textContent = 'I used the getElementBytagName("h3") method to access all of these';
-    
-}
+
+var h3Tags = document.getElementsByTagName('h3');
+if (h3Tags.length > 0) {
+    for (var j = 0; j < h3Tags.length; j++) {
+        h3Tags[j].textContent = 'I used the getElementsByTagName("h3") method to access all of these'
+    }}  
+
 /*----------- Exercise #2: CREATING/APPENDING/INSERTING ELEMENTS/OBJECTS -----------*/
 
 // TODO: Create a paragraph element using this document.createElement() and put this text inside "This node was created using th
-const myPara = document.createElement("p");
-
-
+var parent = document.getElementById('parent'); // Replace 'parent' with the actual ID of the parent node
 
 // TODO: Append the created node to the parent node using the element.appendChild() method
-const parentDiv = document.querySelector("#parent");
-console.log(parentDiv);
-parentDiv.appendChild(myPara);
+var paragraph = document.createElement('p');
+paragraph.textContent = "This node was created using the createElement() method";
+parent.appendChild(paragraph);
 
 // TODO: Create a <a> element using this document.createElement() and put this text inside "I am a <a> tag"
-const myAnchor = document.createElement("a");
-myAnchor.textContent = "I am an <a> tag";
+var link = document.createElement('a');
+link.textContent = "I am a <a> tag";
+parent.insertBefore(link, paragraph);
+
 // BONUS: Add a link href to the <a>
-myAnchor.href = "http://truecoders.io";
+link.href = "http://truecoders.io";
+
 // TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
 parentDiv.insertBefore(myAnchor, myPara);
-/*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
-const newChildPara = document.createdElement("p");
-newChildPara.textContent = "New"
-// TODO: Replace the "Child Node" with a new <p> element that reads "New Child Node"
-const parent = document.getElementById("exercise-container3");
-const nodeToReplace = document.getElementById("N1");
-// TODO: Remove the "New Child Node"
-setTimeout(() =>  {
-    newChildPara.remove();
-}, 2500);
-/*----------- Exercise #4: LIST ITEMS ----------- */
-// Use the following array of values to generate a list on the DOM
 
+/*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/// TODO: Replace the "Child Node" with a new <p> element that reads "New Child Node"
+var parentExercise3 = document.getElementById('exercise3'); // Replace 'exercise3' with the actual ID
+var newParagraph = document.createElement('p');
+newParagraph.textContent = "New Child Node";
+parentExercise3.replaceChild(newParagraph, parentExercise3.childNodes[0]);
+// TODO: Remove the "New Child Node"
+newParagraph.remove();
+/*----------- Exercise #4: LIST ITEMS ----------- */
+// Use the following array of values to generate a list on the DO
 let list = [ "apples", "bananas", "carrots", "dragon fruit", "eggplant", "fish", "grapes", "honey", "ice bag", "juice (any kind)" ];
 
-
 // TODO: Create an unordered list element
-const groceryUL = document.createElement("ul");
+var ul = document.createElement('ul');
 // TODO: Iterate over the array values, and create a list item element for each
-list.forEach((grocery, index, array ) => {
-    const newLI = document.createElement("li");
-    newLI.textContent = grocery;
-}
 
-);
+list.forEach(function(item) {
+    var li = document.createElement('li');
+    li.textContent = item;
+    ul.appendChild(li);
+});
+
 // TODO: Append the new list items to the unordered list element
   groceryUL.append(newLI);
+
+
 // TODO: Append the unordered list to the `div#container` under exercise 4 
-document.getElementById("container").append(groceryUL);
-document.querySelector("#conrainer").append(groceryUL);
+var containerExercise4 = document.getElementById('container'); // Replace 'container' with the actual ID
+containerExercise4.appendChild(ul);
+
 /*----------- Exercise #5: DOM EVENTS --------------*/
 // -> "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user"
 // This div should be a 'modal' that covers the main content on the screen
@@ -91,3 +100,15 @@ const createModalBtn = document.querySelector("#btn");
 //give it a event Listner
 //have listner invoke the show function
 createModalBtn.addEventListener("click" ,show);
+
+   // Function to display the modal
+   function show() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+
+// Function to hide the modal
+function hideModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
